c51a6db57955b77424d5a683f410ff94913d0f46
AndreyXP <andreyapple99@mail.ru>
2026-06-09 12:00:50 +0300

    update

--- MenuBuilder.Abstraction/Model/ColorTypesConstants.cs
+++ MenuBuilder.Abstraction/Model/ColorTypesConstants.cs
@@ -4,55 +4,42 @@ namespace MenuBuilder.Abstraction.Model;
 
 public class ColorTypesConstants
 {
-
-    public static Color ANY = Color.Gray;
-    public static Color BOOL = Color.AliceBlue;
-    public static Color INT8 = Color.DeepSkyBlue;
-    public static Color SINT8 = Color.LightSkyBlue;
-    public static Color INT16 = Color.Cornsilk;
-    public static Color UINT16 = Color.LightCoral;
-    public static Color INT32 = Color.AntiqueWhite;
-    public static Color UINT32 = Color.Aquamarine;
-    public static Color DINT = Color.MediumAquamarine;
-    public static Color UDINT = Color.MediumSeaGreen;
-    public static Color INT64 = Color.Beige;
-    public static Color UINT64 = Color.Aqua;
-    public static Color TIME = Color.DarkTurquoise;
-    public static Color STRING = Color.Moccasin;
-    public static Color REAL = Color.CadetBlue;
-    public static Color DATE = Color.DarkKhaki;
-    public static Color DATETIME = Color.DarkGoldenrod;
-    public static Color NONE = Color.Red;
-    public static Color ANY_NUM = Color.DarkSeaGreen;
-
+    public static readonly Color ANY = Color.FromArgb(0xEF, 0x44, 0x44);
+    public static readonly Color BOOL = Color.FromArgb(0x3B, 0x82, 0xF6);
+    public static readonly Color BYTE = Color.FromArgb(0x10, 0xB9, 0x81);
+    public static readonly Color SBYTE = Color.FromArgb(0x2F, 0xC5, 0x8F);
+    public static readonly Color INT = Color.FromArgb(0xF5, 0x9E, 0x0B);
+    public static readonly Color UINT = Color.FromArgb(0xF6, 0xAE, 0x2C);
+    public static readonly Color DINT = Color.FromArgb(0x8B, 0x5C, 0xF6);
+    public static readonly Color UDINT = Color.FromArgb(0x9D, 0x74, 0xF7);
+    public static readonly Color REAL = Color.FromArgb(0xF1, 0x61, 0x61);
+    public static readonly Color DATE = Color.FromArgb(0x06, 0xB6, 0xD4);
+    public static readonly Color STRING = Color.FromArgb(0x28, 0xC4, 0xDE);
+    public static readonly Color DATETIME = Color.FromArgb(0xEC, 0x48, 0x99);
+    public static readonly Color TIME = Color.FromArgb(0xDB, 0x27, 0x77);
+    public static readonly Color NONE = Color.FromArgb(0x9C, 0xA3, 0xAF);
 
     public static Dictionary<string, Color> ColorsConvert = new()
     {
         { AllowedTypeConstants.ANY, ANY },
         { AllowedTypeConstants.BOOL, BOOL },
-        { AllowedTypeConstants.SHORT, INT16 },
-        { AllowedTypeConstants.USHORT, INT16 },
-        { AllowedTypeConstants.INT16, INT16 },
-        { AllowedTypeConstants.INT32, INT32 },
-        { AllowedTypeConstants.INT, INT32 },
-        { AllowedTypeConstants.UINT32, UINT32 },
-        { AllowedTypeConstants.UINT, UINT32 },
-        { AllowedTypeConstants.INT64, INT64 },
-        { AllowedTypeConstants.LONG, INT64 },
-        { AllowedTypeConstants.UINT64, UINT64 },
-        { AllowedTypeConstants.Ulong, UINT64 },
-        { AllowedTypeConstants.TIME, TIME },
-        {AllowedTypeConstants.STRING, STRING },
+        { AllowedTypeConstants.SHORT, INT },
+        { AllowedTypeConstants.USHORT, UINT },
+        { AllowedTypeConstants.INT16, INT },
+        { AllowedTypeConstants.INT, INT },
+        { AllowedTypeConstants.UINT32, UINT },
+        { AllowedTypeConstants.UINT, UINT },
         { AllowedTypeConstants.REAL, REAL },
         { AllowedTypeConstants.FLOAT, REAL },
         { AllowedTypeConstants.DATE, DATE },
-        { AllowedTypeConstants.NONE, NONE },
-        { AllowedTypeConstants.ANY_NUM, ANY_NUM },
-         { AllowedTypeConstants.BYTE, INT8 },
-         { AllowedTypeConstants.SBYTE, SINT8 },
+         { AllowedTypeConstants.BYTE, BYTE },
+         { AllowedTypeConstants.SBYTE, SBYTE },
          { AllowedTypeConstants.DATETIME, DATETIME },
          { AllowedTypeConstants.DT, DATETIME },
+         { AllowedTypeConstants.TIME, TIME },
          { AllowedTypeConstants.DINT, DINT },
          { AllowedTypeConstants.UDINT, UDINT },
+         { AllowedTypeConstants.STRING, STRING },
+         {AllowedTypeConstants.NONE, NONE}
     };
 }
